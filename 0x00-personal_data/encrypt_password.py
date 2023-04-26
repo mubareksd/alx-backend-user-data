29412105577c28e69ad689572b68e895be69aa43
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-27 00:35:14 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,16 @@ def hash_password(password: str) -> bytes:
         bytes: salted hashed password, which is a byte string
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid function that expects 2 arguments and returns a boolean
+
+    Args:
+        hashed_password (bytes): salted hashed password
+        password (str): string to check
+
+    Returns:
+        bool: True if the password is valid, False otherwise
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
