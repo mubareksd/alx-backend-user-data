b2c2f75f16953b3efaeb2d0817613f64735333a4
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-26 23:17:35 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,18 +5,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """filtered_logger function that,
-    given a list of fields, replaces them with a redaction
-
-    Args:
-        fields (list): list of strings representing
-        all fields to obfuscate
-        redaction (str): string representing by
-        what the field will be obfuscated
-        message (str): string representing the log line
-
-    Returns:
-        str: log message obfuscated
+    """given a list of fields, replaces them with a redaction
     """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
