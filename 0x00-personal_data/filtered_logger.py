83b12a33470690cef8e1e34c9de9fc63ca59cfcb
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-26 23:30:52 +0300

    task q

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """filtered_logger module
 """
 import re
+import logging
 from typing import List
 
 
@@ -24,3 +25,34 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init function
+
+        Args:
+            fields (list): list of strings representing
+        """
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format function that filter values in incoming log records
+        using filter_datum function
+
+        Args:
+            record (logging.LogRecord): log record
+
+        Returns:
+            str: log message obfuscated
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
