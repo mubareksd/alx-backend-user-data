c160ffec77168bbac1092cf54a94810cfb41934c
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-02 18:20:21 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth module for the API
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -19,9 +20,33 @@ class BasicAuth(Auth):
         Returns:
             str: _description_
         """
-        if authorization_header is None or \
-            type(authorization_header) is not str or \
-                not authorization_header.startswith('Basic '):
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
             return None
 
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """_summary_
+
+        Args:
+            base64_authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode('utf-8')
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
