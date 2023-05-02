ac47087346ae556f5e8412d4add7d82e89751456
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-02 11:44:07 +0300

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,20 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """_summary_
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if authorization_header is None or \
+            type(authorization_header) is not str or \
+                not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split()[1]
