0ff0521f0321572b2d9ae5521d0b5e7b94e5e616
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-03 19:19:55 +0300

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,9 +63,8 @@ def before_request():
         ]
 
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request):
-                abort(401)
-            if auth.session_cookie(request):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
