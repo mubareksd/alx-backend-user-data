d0f76f1f5df3d4abadac8ab7f6d645be3a047c45
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-04 21:31:39 +0300

    updtae

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,7 +32,7 @@ def auth_session_login():
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/api/v1/auth_session/logout',
+@app_views.route('/auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     """ DELETE /api/v1/auth_session/logout
