28c43ff56243d4a0476749a851365aa1457ef503
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-09 20:39:09 +0300

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,4 +14,4 @@ def _hash_password(password):
     Returns:
         bytes: hashed password
     """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
