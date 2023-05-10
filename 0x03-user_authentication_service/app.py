eb18356eb30182df0ffc793f12588f0df55b77b6
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-10 12:33:41 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -114,7 +114,7 @@ def update_password():
     new_password = request.form.get("new_password")
 
     try:
-        Auth.update_password(reset_token, new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": f"{email}", "message": "Password updated"})
     except ValueError:
         abort(403)
