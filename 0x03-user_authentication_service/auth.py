4433f3d58eda1af83de2decf5796a1a3397778fb
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-10 12:16:42 +0300

    task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -136,3 +136,10 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, password=_hash_password(password))
+        except NoResultFound:
+            raise ValueError
