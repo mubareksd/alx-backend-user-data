574bf264f6366d47e8a0002a07d1c8db40c6fa47
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-10 10:05:54 +0300

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,20 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """method that create user session
+
+        Args:
+            email (str): user email
+
+        Returns:
+            str: session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
